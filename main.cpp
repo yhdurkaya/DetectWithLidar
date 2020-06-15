974e38ce9ea472cccbca9b1e5cb1b5b53002d34e
Yusuf H. Durkaya <yusufdurkaya@gmail.com>
2020-06-15 16:21:54 +0300

    Outlier detection using RANSAC
    Signed-off-by: Yusuf H. Durkaya <yusufdurkaya@gmail.com>

--- main.cpp
+++ main.cpp
@@ -7,10 +7,14 @@
 #include <pcl/point_cloud.h>
 #include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/sample_consensus/ransac.h>
+#include <pcl/sample_consensus/sac_model_plane.h>
 
 int main() {
 
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr inlierCloud (new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr outlierCloud (new pcl::PointCloud<pcl::PointXYZ>);
 
     if (pcl::io::loadPCDFile<pcl::PointXYZ> ("../test_files/sdc.pcd", *cloud) == -1) //* load the file
     {
@@ -19,29 +23,46 @@ int main() {
     }
 
     pcl::visualization::PCLVisualizer viewer("Original viewer");
-    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> sourceCloudColorHandler(cloud, 255, 255, 255);
-    viewer.addPointCloud(cloud, sourceCloudColorHandler, "Original Cloud");
-
-    std::cout << cloud->size() << "\n";
 
     pcl::VoxelGrid<pcl::PointXYZ> voxelGrid;
     voxelGrid.setInputCloud(cloud);
     voxelGrid.setLeafSize(0.5, 0.5, 0.5);
     voxelGrid.filter(*cloud);
 
-    pcl::visualization::PCLVisualizer viewer2("Downsampled viewer");
-    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> downsampledCloudColorHandler (cloud, 255, 255, 255); // Red
-    viewer2.addPointCloud (cloud, downsampledCloudColorHandler, "Downsampled Cloud");
+    std::vector<int> inliers;
+    std::vector<int> outliers;
 
-    std::cout << cloud->size() << "\n";
+    pcl::SampleConsensusModelPlane<pcl::PointXYZ>::Ptr modelPlane (new pcl::SampleConsensusModelPlane<pcl::PointXYZ> (cloud));
 
+    pcl::RandomSampleConsensus<pcl::PointXYZ> ransac (modelPlane);
+    ransac.setDistanceThreshold (0.25);
+    ransac.computeModel();
+    ransac.setMaxIterations(150);
+    ransac.getInliers(inliers);
+
+    pcl::copyPointCloud (*cloud, inliers, *inlierCloud);
+
+    for(int i =0; i<cloud->size(); ++i){
+        if(std::find(inliers.begin(), inliers.end(), i) != inliers.end()){
+
+        }
+        else{
+            outliers.push_back(i);
+        }
+    }
+    pcl::copyPointCloud(*cloud, outliers, *outlierCloud);
+
+    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> inlierCloudColorHandler (inlierCloud, 0, 255, 0); // Red
+    viewer.addPointCloud (inlierCloud, inlierCloudColorHandler, "Inlier Cloud");
+
+    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> outlierCloudColorHandler (outlierCloud, 255, 0, 0); // Red
+    viewer.addPointCloud (outlierCloud, outlierCloudColorHandler, "Outlier Cloud");
 
     while(!viewer.wasStopped()){
         viewer.spinOnce();
     }
 
-    while(!viewer2.wasStopped()){
-        viewer.spinOnce();
-    }
-
+    std::cout << cloud->size() << "\n";
+    std::cout << inlierCloud->size() << "\n";
+    std::cout << outlierCloud->size() << std::endl;
 }
